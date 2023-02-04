ea90da765e83c12cc7c272ee68eae55300ac5dab
pedroelir <pedro_r_b777@hotmail.com>
2023-02-04 19:11:52 +0100

    Corrected spacing and print errors

--- netadapter.py
+++ netadapter.py
@@ -1,18 +1,35 @@
 import subprocess
+import time
 
 
 def enable_netadapter(adapter_name: str) -> bool:
-    cmd: list[str] = ["powershell.exe", f"Enable-NetAdapter -Name{adapter_name} ; exit -not ($?)"]
+    cmd: list[str] = ["powershell.exe", f"Enable-NetAdapter -Name {adapter_name} ; exit -not ($?)"]
     process: subprocess.CompletedProcess = subprocess.run(cmd, capture_output=True, text=True, timeout=10)
-    return bool(process.returncode == 0)
+    if bool(process.returncode == 0):
+        return True
+    print(process.stdout)
+    print(process.stderr)
+    return False
 
 
 def disable_netadapter(adapter_name: str) -> bool:
-    cmd: list[str] = ["powershell.exe", f"Disable-NetAdapter -Name{adapter_name} ; exit -not ($?)"]
+    cmd: list[str] = ["powershell.exe", f"Disable-NetAdapter -Name {adapter_name} -Confirm:$False; exit -not ($?)"]
     process: subprocess.CompletedProcess = subprocess.run(cmd, capture_output=True, text=True, timeout=10)
-    return bool(process.returncode == 0)
+    if bool(process.returncode == 0):
+        return True
+    print(process.stdout)
+    print(process.stderr)
+    return False
 
 
-if __name__ == "__main":
-    disable_netadapter("Ethernet")
-    enable_netadapter("Ethernet")
+if __name__ == "__main__":
+    if disable_netadapter("Ethernet"):
+        print("Adapter disabled")
+    else:
+        print("Adapter not disabled")
+
+    time.sleep(10)
+    if enable_netadapter("Ethernet"):
+        print("Adapter enabled")
+    else:
+        print("Adapter not enabled")
