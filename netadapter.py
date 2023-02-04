eeaed7c0606097ea7aa7aad4d514766e4a77e73b
pedroelir <pedro_r_b777@hotmail.com>
2023-02-04 19:36:45 +0100

    add get_netadapters

--- netadapter.py
+++ netadapter.py
@@ -22,14 +22,29 @@ def disable_netadapter(adapter_name: str) -> bool:
     return False
 
 
-if __name__ == "__main__":
-    if disable_netadapter("Ethernet"):
-        print("Adapter disabled")
-    else:
-        print("Adapter not disabled")
+def get_netadapters():
+    cmd: list[str] = ["powershell.exe", "(Get-NetAdapter).Name; exit -not ($?)"]
+    process: subprocess.CompletedProcess = subprocess.run(cmd, capture_output=True, text=True, timeout=10)
+    if bool(process.returncode == 0):
+        print(process.stdout)
+        adapters = process.stdout.split("\n")
+        adapters.pop()
+        return adapters
+    print(process.stdout)
+    print(process.stderr)
+    return []
 
-    time.sleep(10)
-    if enable_netadapter("Ethernet"):
-        print("Adapter enabled")
-    else:
-        print("Adapter not enabled")
+
+if __name__ == "__main__":
+    # if disable_netadapter("Ethernet"):
+    #     print("Adapter disabled")
+    # else:
+    #     print("Adapter not disabled")
+
+    # time.sleep(10)
+    # if enable_netadapter("Ethernet"):
+    #     print("Adapter enabled")
+    # else:
+    #     print("Adapter not enabled")
+    adapters = get_netadapters()
+    print(adapters)
